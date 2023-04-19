04d23eee10bc371fd0c36c94234998d4320c2b21
Khrista K <khristakhwaja@gmail.com>
2023-04-19 14:13:01 -0700

    wireframe

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <form className="d-flex m-2">
+        <label htmlfor="todo" className="m-2">Add to your To-Do list</label>
+        <input className="form-control" name="" type="text"/>
+        <input type="submit" value="Add" className="btn btn-primary m-2" />
+      </form>
+      <div>
+        <table className="table">
+          <thead>
+            <th scope="col">#</th>
+            <th scope="col">To-Do</th>
+            <th scope="col">Status</th>
+            <th scope="col">Action</th>
+          </thead>
+          <tbody>
+            <td>ID</td>
+            <td>To-Do</td>
+            <td>
+              <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+            </td>
+            <td>
+              <button type="button" className="btn-sm btn btn-dark bg-dark">Delete</button>
+            </td>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
